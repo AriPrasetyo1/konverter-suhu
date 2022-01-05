5b5fa9c86505f7d3f87388d913b7509abcc1056b
AriPrasetyo1 <prasetyoari600@gmail.com>
2022-01-05 15:11:59 +0700

    Konversi Suhu

---
+++ src/main/java/konverter/suhu/suhu/SuhuApplication.java
@@ -0,0 +1,13 @@
+package konverter.suhu.suhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konverter/suhu/suhu/SuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package konverter.suhu.suhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
